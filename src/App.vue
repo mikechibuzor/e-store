6e579f93cbff3ac0720ea60aa236e9c6c51e9380
mikechibuzor <iwuagwuchibuzor@gmail.com>
2021-12-20 12:47:44 +0100

    first commit, project environment setup and tools configuration

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper px-32">
+    <router-link to="/home" class="text-black font-Kumbh-Sans text-3xl">Create new</router-link>
+    <router-view v-slot="slotProps">
+        <transition name="route" mode="in-out">
+          <component :is="slotProps.Component"></component>
+        </transition>
+    </router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+.route-enter-from{
+  opacity: 0;
+  transform: translateY(-.4rem);
+}
+.route-leave-to{
+  opacity: 0;
+  transform: translateY(4rem);
+}
+.route-enter-active{
+  transition: all .3s ease-out;
+}
+.route-leave-active{
+    transition: all .3s ease-in;
+}
+.route-enter-to,
+.route-leave-from{
+  opacity: 1;
+  transform: translateY(0);
+}
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-orange-1": "hsl(26, 100%, 55%)",
+        "primary-orange-pale": "hsl(25, 100%, 94%)",
+        "very-dark-blue": "hsl(220, 13%, 13%)",
+        "dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "grayish-blue": "hsl(220, 14%, 75%)",
+        "light-grayish-blue": "hsl(223, 64%, 98%)",
+      },
+      fontFamily: {
+        "Kumbh-Sans": ["Kumbh Sans", "sans-serif"],
+      },
+      height: {
+        height10: "10vh",
+        height15: "15vh",
+        height20: "20vh",
+        height30: "30vh",
+        height40: "40vh",
+        height50: "50vh",
+        height55: "55vh",
+        height60: "60vh",
+        height70: "70vh",
+        height80: "80vh",
+        height90: "90vh",
+        height100: "100vh",
+      },
+      screens: {
+        "custom-breakpoint": "1440px",
+        "custom-breakpoint1": "1600px",
+        "custom-breakpoint2": "1900px",
+      },
+    },
+  },
+  plugins: [],
+};
