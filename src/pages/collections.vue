b3ea0abcc34397f5fa2e0e017ef2bcb29666c6ab
mikechibuzor <iwuagwuchibuzor@gmail.com>
2021-12-22 13:51:53 +0100

    worked out the logic to use fetch data

--- src/pages/collections.vue
+++ src/pages/collections.vue
@@ -1,20 +1,34 @@
 <template>
-  <p>Collections page</p>
+  <p>{{ state.title  }}</p>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, reactive } from 'vue';
 import { useStore } from 'vuex';
 
+// accessing store
 const store = useStore();
 
-onMounted(() => {
-     store.dispatch('products/getAllProducts');
-    //  console.log(store);
-    //  setTimeout(()=>{
-    //    console.log(store.getters['products/getProducts']);
-    //  },3000)
-    })
+// reactive variables
+const state = reactive({
+  title: null,
+})
+
+// computed methods
+// const dispData = computed( ()=> store.getters['products/getProducts'][0].title );
+
+// onMounted(() => {
+//      store.dispatch('products/getAllProducts');
+//     //  console.log(store);
+//      setTimeout(()=>{
+//        console.log(store.getters['products/getProducts'][0].title);
+//      },3000)
+//     })
+onMounted( 
+  async ()=>{
+    await store.dispatch('products/getAllProducts');
+    state.title = store.getters['products/getProducts'][0].title;
+  })
 </script>
 
 <style>
